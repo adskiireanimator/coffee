91a76e754a6a74d2c672f88ec5c99f2fd5df3ea3
adskiireanimator <mihaildorodnikov032@gmail.com>
2023-01-02 02:39:14 +0300

    add a little bit of adaptive

---
+++ main.js
@@ -0,0 +1,14 @@
+/*const burgermenu = document.getElementById("burgermenu");
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+
+burgermenu.addEventListener(
+  "click",
+  () => {
+    alert(25);
+    line1.style.transform = "rotate(45deg)";
+  },
+  false
+);
+*/
